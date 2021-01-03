e0c31f0fd91e36b90c0b271199bc5a6e04257450
Notker367 <notstrauss@gmail.com>
2021-01-03 19:17:51 +0300

    0.01.2
    Добавил создание обектов и движение

--- Assets/Scripts/CreatObject.cs
+++ Assets/Scripts/CreatObject.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class CreatObject : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject[] objects;
+    private float crtTime;
     void Start()
     {
+        StartCoroutine("creatObj");
         
+    }
+    IEnumerator creatObj ()
+    {
+        while(true)
+        {
+            crtTime = Random.Range(5f,9f);
+            if(crtTime < 8f)
+            {
+                int rdmObj = Random.Range(0, objects.Length);
+                Instantiate (objects[rdmObj], this.transform.position, objects[rdmObj].transform.rotation);
+            }
+            yield return new WaitForSeconds(crtTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
